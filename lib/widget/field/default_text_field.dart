9892cd3a630f88a7e51da69bacb4ea135ecdf4c6
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2022-10-29 16:34:58 +0700

    🎨 Added common widget

---
+++ lib/widget/field/default_text_field.dart
@@ -0,0 +1,127 @@
+import 'package:bank_sampah/constant/app_colors.dart';
+import 'package:bank_sampah/constant/app_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class DefaultTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String hintText;
+  final String? labelText;
+  final int? maxLength;
+  final TextInputType? inputType;
+  final bool initial;
+  final String? initialText;
+  final bool isObscure;
+  final bool alignRight;
+  final ValueChanged<String?>? onSaved;
+  final ValueChanged<String>? onChanged;
+  final FormFieldValidator<String>? validator;
+  final Widget? suffixWidget;
+  final BoxConstraints? suffixConstraints;
+  final bool isEnabled;
+  final bool readOnly;
+  final bool needAutoValidate;
+  final int maxLines;
+
+  const DefaultTextField({
+    Key? key,
+    required this.hintText,
+    this.onSaved,
+    this.controller,
+    this.labelText,
+    this.maxLength,
+    this.inputType = TextInputType.text,
+    this.initial = false,
+    this.initialText,
+    this.isObscure = false,
+    this.alignRight = false,
+    this.onChanged,
+    this.validator,
+    this.suffixWidget,
+    this.suffixConstraints,
+    this.isEnabled = true,
+    this.readOnly = false,
+    this.needAutoValidate = false,
+    this.maxLines = 1,
+  }) : super(key: key);
+
+  // listInputType
+  // 0 : text
+  // 1 : multiline
+  // 2 : number
+  // 3 : phone
+  // 4 : datetime
+  // 5 : emailAddress
+  // 6 : url
+  // 7 : visiblePassword
+  // 8 : name
+  // 9 : streetAddress
+  // 10 : none
+
+  List<TextInputFormatter> get _inputFormatters {
+    List<TextInputFormatter> _formatters = <TextInputFormatter>[];
+    switch (inputType?.index) {
+      case 2:
+        _formatters.add(FilteringTextInputFormatter.digitsOnly);
+        break;
+      case 3:
+        _formatters.add(FilteringTextInputFormatter.digitsOnly);
+        break;
+      case 6:
+        _formatters.add(FilteringTextInputFormatter.allow(RegExp(" ")));
+        break;
+      case 9:
+        break;
+    }
+    return _formatters;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      enabled: isEnabled,
+      readOnly: readOnly,
+      controller: controller,
+      onChanged: onChanged ?? (value) {},
+      textAlign: alignRight ? TextAlign.end : TextAlign.start,
+      cursorColor: AppColors.greenSheen,
+      initialValue: initialText,
+      keyboardType: inputType,
+      inputFormatters: _inputFormatters,
+      obscureText: isObscure,
+      style: AppText.text12,
+      maxLines: maxLines,
+      autovalidateMode: needAutoValidate ? AutovalidateMode.always : null,
+      decoration: InputDecoration(
+        focusColor: AppColors.greenSheen,
+        fillColor: AppColors.lightGrey,
+        filled: false,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: const BorderSide(color: AppColors.lightGrey),
+        ),
+        hintText: hintText,
+        hintStyle: AppText.text12.copyWith(
+          color: AppColors.lightGrey,
+        ),
+        labelText: labelText,
+        labelStyle: AppText.text12,
+        floatingLabelStyle: AppText.text12.copyWith(
+          color: AppColors.greenSheen,
+        ),
+        contentPadding: const EdgeInsets.all(16),
+        suffixIcon: suffixWidget,
+        suffixIconConstraints: suffixConstraints,
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.white,
+          ),
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+      maxLength: maxLength,
+      validator: validator,
+      onSaved: onSaved ?? (value) {},
+    );
+  }
+}
